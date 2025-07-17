09fb067b0bbd9290325be55f141df702e4cb27f4
Vishal <vssule8@gmail.com>
2025-07-17 20:17:11 +0530

    code

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(<App />);
+} else {
+  throw new Error("Root element not found");
+}
