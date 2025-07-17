b9847e5caf772cf731f6e86e1311033f1b9c2e5c
Vishal <vssule8@gmail.com>
2025-07-17 20:26:14 +0530

    code

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/robo-vision-site/" : "/",
+  base: mode === "production" ? "/test/" : "/",
   server: {
     host: "::",
     port: 8080,
