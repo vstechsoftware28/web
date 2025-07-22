4389c4a52124d2c966faf3fb126d7f1712a46637
Vishal <vssule8@gmail.com>
2025-07-22 13:44:16 +0530

    code

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/web/" : "/",
+  base: "/web/",
   server: {
     host: "::",
     port: 8080,
